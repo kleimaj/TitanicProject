b17cf14af7c8639b2f9fe76e42286e397da25caf
Kleiman <@wwu.edu>
2019-02-01 16:59:11 -0800

    milestone1.R updated

--- milestone1.R
+++ milestone1.R
@@ -3,5 +3,5 @@ titanic_network <- read.csv("titanic.csv",
                           header = TRUE)
 #add edges
 edgelist <- read.csv("edgelist.txt", header = FALSE, sep = "\t")
-g <- graph.data.frame(titanic_network)
-plot(g)
+g <- graph.data.frame(edgelist, directed = FALSE)
+plot(g, vertex.size = 1, vertex.label = "")
